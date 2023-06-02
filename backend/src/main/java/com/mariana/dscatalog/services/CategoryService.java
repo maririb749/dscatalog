d6bcce95583c9b21efc223473b123ef22677fac5
Mariana Santos <maririb51@gmail.com>
2023-06-02 13:18:38 +0100

    DTO

--- backend/src/main/java/com/mariana/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/mariana/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.mariana.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mariana.dscatalog.dto.CategoryDTO;
 import com.mariana.dscatalog.entities.Category;
 import com.mariana.dscatalog.repositories.CategoryRepository;
 
@@ -18,9 +20,13 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+	    List <Category>list = repository.findAll();
+	    return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	    	
+	    }
+  
 		
 	}
 
-}
+
