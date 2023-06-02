b6fd1814e6d0d40cd2fe1a226641a9e9e4aba5cd
Mariana Santos <maririb51@gmail.com>
2023-06-02 11:48:46 +0100

    Database Seeding

--- backend/src/main/java/com/mariana/dscatalog/entities/Category.java
+++ backend/src/main/java/com/mariana/dscatalog/entities/Category.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -16,7 +17,7 @@ public class Category implements Serializable {
 	
 	
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
@@ -24,30 +25,43 @@ public class Category implements Serializable {
 		
 	}
 
+	
 	public Category(Long id, String name) {
+		this.id = id;
 		this.name = name;
 	}
+	
 
 	public Long getId() {
 		return id;
 	}
 
+
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+
 
 	public String getName() {
 		return name;
 	}
 
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -59,6 +73,12 @@ public class Category implements Serializable {
 			return false;
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	
+	@Override
+	public String toString() {
+	    return "Category [id=" + id + ", name=" + name + "]";
 	}
 
 
